ff3d184792085f7585c3e11189dd3ad59a8cf88e
slolo <mustaphason1991@outlook.fr>
2019-11-26 16:36:41 +0100

    Installing bootstrap/react-router-dom

--- src/index.js
+++ src/index.js
@@ -2,7 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App';
 import * as serviceWorker from './serviceWorker';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './styles/global.scss';
+
 ReactDOM.render(<App />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
